f2dc8a438258a7178e4b95f0b73a99425fe08ec2
kaur1081 <karishmawalia@gmail.com>
2019-10-05 12:04:02 -0700

    get and post routes for rovers

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -21,4 +21,18 @@ module.exports = function(app) {
       res.json(dbExample);
     });
   });
+  // nasa api search
+  app.post("/api/rovers", function(req, res) {
+    // example variable for date manipulation in axios call
+    // date = currentDate;
+    // wrap axios call in forLoop
+    //Create call on get route for rovers page
+    req.axios
+      .get(
+        "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-2-8&api_key=Yg0nubAuazdBXPOMSsk7GcCa4wjJjAIaYVSBjB78"
+      )
+      .then(function(data) {
+        return res.render("rovers", data);
+      });
+  });
 };
--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.engine(
     defaultLayout: "main"
   })
 );
+
 app.set("view engine", "handlebars");
 
 // Routes

a193cdee34c396e2490968f626ffb5ffcf96a1b2
kaur1081 <karishmawalia@gmail.com>
2019-10-09 21:38:23 -0700

    mars branch

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 var db = require("../models");
-var axios = require("axios");
+
 
 module.exports = function(app) {
   // Get all examples
@@ -8,31 +8,4 @@ module.exports = function(app) {
       res.json(dbExamples);
     });
   });
-
-  // Create a new example
-
-  app.get("/api/rovers/:id/images", function(req, res) {
-    // get route params
-    // check for query arguments
-    var roverId = req.params.id;
-    console.log(roverId);
-    // var date = req.params.date;
-    // for (var i = 0; i < data.length; i++) {
-    //   if (chosen === data[i].roverId) {
-    //     return res.json(data[i]);
-    //   }
-    // }
-    // res.json(data);
-    // build our query to the db or build a URL to make a request to an external api for this data
-    axios
-      .get(
-        "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=Yg0nubAuazdBXPOMSsk7GcCa4wjJjAIaYVSBjB78"
-      )
-      .then(function(data) {
-
-        console.log(data.data.photos);
-        return res.render("rovers", data);
-      });
-  });
-  
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,10 @@
 var db = require("../models");
+var axios = require("axios");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
@@ -12,19 +13,55 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
+  app.get("/example/:id", function (req, res) {
     db.Example.findOne({
       where: {
         id: req.params.id
       }
-    }).then(function(dbExample) {
+    }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
     });
   });
-  app.get("/rovers", function(req, res) {
-    res.render("rovers");
+  // app.get("/rovers", function(req, res) {
+  //   res.render("rovers");
+  // });
+
+  // nasa api search
+  app.get("/rovers", function (req, res) {
+    //Create call on get route for rovers page
+    axios
+      .get(
+        "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-2-8&api_key=Yg0nubAuazdBXPOMSsk7GcCa4wjJjAIaYVSBjB78"
+      )
+      .then(function (data) {
+        console.log("rovers", data.data.photos);
+        // return res.json(data.data.photos);
+        return res.render("rovers", data.data);
+      });
+  });
+
+  app.get("/api/mars", function (req, res) {
+    // get route params
+    var mars = req.params.id;
+    console.log(mars);
+    // check for query arguments
+    // res.json(data);
+    // build our query to the db or build a URL to make a request to an external api for this data
+    axios
+      .get(
+        "https://api.nasa.gov/insight_weather/?api_key=Yg0nubAuazdBXPOMSsk7GcCa4wjJjAIaYVSBjB78&feedtype=json&ver=1.0"
+      )
+      .then(function (data) {
+        console.log(data.data);
+        return res.render("mars", data.data);
+      });
+  });
+
+  // render mars page
+  app.get("/mars", function (req, res) {
+    res.render("mars");
   });
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {

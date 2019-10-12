0192f01169f088a6ffa2d9541b2713a6a8035ea2
Keila Vizcarra <keilavizcarra11@gmail.com>
2019-10-12 09:23:24 -0700

    button edit

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,10 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable quotes */
 // Get references to page elements
 var $exampleText = $("#example-text");
 var $exampleDescription = $("#example-description");
 var $submitBtn = $("#submit");
 var $exampleList = $("#example-list");
-
 // The API object contains methods for each kind of request we'll make
 var API = {
   saveExample: function(example) {
@@ -29,77 +30,61 @@ var API = {
     });
   }
 };
-
 // refreshExamples gets new examples from the db and repopulates the list
-// var result = function() {
+// var refreshExamples = function() {
 //   API.getExamples().then(function(data) {
-//     var $examples = data.id(function(result) {
+//     var $examples = data.map(function(example) {
 //       var $a = $("<a>")
 //         .text(example.text)
 //         .attr("href", "/example/" + example.id);
-
 //       var $li = $("<li>")
 //         .attr({
 //           class: "list-group-item",
 //           "data-id": example.id
 //         })
 //         .append($a);
-
 //       var $button = $("<button>")
 //         .addClass("btn btn-danger float-right delete")
 //         .text("ｘ");
-
 //       $li.append($button);
-
 //       return $li;
 //     });
-
 //     $exampleList.empty();
 //     $exampleList.append($examples);
 //   });
 // };
-
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
 var handleFormSubmit = function(event) {
   event.preventDefault();
-
   var example = {
     text: $exampleText.val().trim(),
     description: $exampleDescription.val().trim()
   };
-
   if (!(example.text && example.description)) {
     alert("You must enter an example text and description!");
     return;
   }
-
   API.saveExample(example).then(function() {
     refreshExamples();
   });
-
   $exampleText.val("");
   $exampleDescription.val("");
 };
-
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
 var handleDeleteBtnClick = function() {
   var idToDelete = $(this)
     .parent()
     .attr("data-id");
-
   API.deleteExample(idToDelete).then(function() {
     refreshExamples();
   });
 };
-
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
 
-
 //Picture of the day
 var mySpaceImg = $('<img alt="image">').attr('src', 'https://apod.nasa.gov/apod/image/1901/sombrero_spitzer_1080.jpg');
-
 $('#myPictureArea').append(mySpaceImg);
